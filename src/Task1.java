a9a3b8fd500f5de5640d019412e1708aace0258c
Daniel <scott2dp@mail.uc.edu>
2025-06-26 11:18:06 +1000

    Tasks 1 and 2

---
+++ src/Task1.java
@@ -0,0 +1,12 @@
+public class Task1 { // Class name changed to Task1
+
+    public static void main(String[] args) {
+        System.out.println("Task 1: Count up by 1 from 0 to 30");
+        System.out.println("-----------------------------------");
+
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n"); // Add a newline for better spacing for next task
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,12 @@
+public class Task2 { // New class for Task 2
+
+    public static void main(String[] args) {
+        System.out.println("Task 2: Count down by 1 from 30 to 0");
+        System.out.println("------------------------------------");
+
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n"); // Add a newline for better spacing for next task
+    }
+}
